27d00a446b94ba83ee80621ba0483815b6baf0bf
Ndurue <emmandurue@gmail.com>
2024-07-27 00:49:29 +0100

    Update RandomString.vue

--- src/components/RandomString.vue
+++ src/components/RandomString.vue
@@ -3,9 +3,14 @@
 </script>
 
 <template>
-    <div class="text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium, doloribus! Tenetur hic beatae cumque architecto <span style="font-weight: bold;">Ndurue</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptates voluptatem architecto explicabo ea accusamus!.</div>
+    <div class="text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium, doloribus! Tenetur hic
+        beatae cumque architecto 
+        <br>
+        <span style="font-weight: bold;">Ndurue</span>
+         <br>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptates voluptatem architecto explicabo ea
+        accusamus!.
+    </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
