286f4152412ffc538114653e8bce6c2e38794480
Ndurue <emmandurue@gmail.com>
2024-07-26 22:00:34 +0100

    Switched to hash mode for routing

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # RouteProject
  Project to get familiarized with routing in vue
+
+ # To update the docs folder using git so your changes can reflect on your github website
+ # Rebuild the project
+npm run build
+
+# Clear the existing docs folder
+rm -rf docs/*
+
+# Move the new build files to the docs folder
+mv dist/* docs/
+
+# Commit and push the changes
+git add docs
+git commit -m "Deploy updated build to GitHub Pages"
+git push origin main
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import HomeView from '../views/FirstPage.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL), // Switch to hash mode
+
   routes: [
     {
       path: '/',
