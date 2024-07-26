072305a60ac901b0299b80a68b21f32ed7c12697
Ndurue <emmandurue@gmail.com>
2024-07-26 20:58:52 +0100

    Test to center content on mobile

--- src/components/RandomString.vue
+++ src/components/RandomString.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <div class="text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium, doloribus! Tenetur hic beatae cumque architecto.</div>
+    <div class="text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium, doloribus! Tenetur hic beatae cumque architecto <span style="font-weight: bold;">Ndurue</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptates voluptatem architecto explicabo ea accusamus!.</div>
 </template>
 
 <style scoped>
