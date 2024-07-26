978819ad8c1e3853c16bc0d7d7baec19800458a6
Ndurue <emmandurue@gmail.com>
2024-07-26 22:37:23 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,8 @@ import HomeView from '../views/FirstPage.vue'
 
 const router = createRouter({
   // history: createWebHistory(import.meta.env.BASE_URL),
-  history: createWebHashHistory(import.meta.env.BASE_URL), // Switch to hash mode
+  // history: createWebHashHistory(import.meta.env.BASE_URL), // Switch to hash mode
+  history: createWebHashHistory('/RouteProject/'), // Switch to hash mode
 
   routes: [
     {
