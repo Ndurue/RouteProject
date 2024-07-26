9857d48113069226d28faf6ab46e2d94730853f8
Ndurue <emmandurue@gmail.com>
2024-07-27 00:45:33 +0100

    Update on router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import HomeView from '../views/FirstPage.vue'
+import SecondPage from '../views/SecondPage.vue'
+import ThirdPage from '../views/ThirdPage.vue'
+import FourthPage from '../views/FourthPage.vue'
+
 
 const router = createRouter({
   // history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,17 +22,17 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/SecondPage.vue')
+      component: SecondPage
     },
     {
       path: '/thirdpage',
       name: 'thirdpage',
-      component: () => import('../views/ThirdPage.vue')
+      component: ThirdPage
     },
     {
       path: '/fourthpage',
       name: 'fourthpage',
-      component: () => import('../views/FourthPage.vue')
+      component: FourthPage
     },
   ]
 })
--- src/views/ThirdPage.vue
+++ src/views/ThirdPage.vue
@@ -6,18 +6,19 @@ console.log('Third Page Loaded');
 
 <template>
     <div class="container">
+
         <RouterLink to="/">
             <div class="btn">First Page</div>
         </RouterLink>
+
         <RouterLink to="/secondpage">
             <div class="btn">Second Page</div>
         </RouterLink>
+
         <RouterLink to="/fourthpage">
             <div class="btn">Fourth Page</div>
         </RouterLink>
-
-
-
+        
     </div>
 </template>
 
