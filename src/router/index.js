84a234262759f6b0d7fc510621eb12e803490360
Ndurue <emmandurue@gmail.com>
2024-07-27 00:53:07 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -6,9 +6,9 @@ import FourthPage from '../views/FourthPage.vue'
 
 
 const router = createRouter({
-  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   // history: createWebHashHistory(import.meta.env.BASE_URL), // Switch to hash mode
-  history: createWebHashHistory('/RouteProject/'), // Switch to hash mode
+  // history: createWebHashHistory('/RouteProject/'), // Switch to hash mode
 
   routes: [
     {
